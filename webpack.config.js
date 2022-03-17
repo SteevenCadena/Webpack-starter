4bac4064742982daf147ffe799ec6e54fd08407f
Carlos <steevencadena49@gmail.com>
2022-03-17 13:44:00 -0500

    Primer commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpack    = require("html-webpack-plugin");
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin     = require("copy-webpack-plugin");
+
+module.exports = {
+
+    mode: 'development',//elejir modo
+
+    output:{
+        clean: true
+    },
+
+    module: {
+        rules:[
+            {//barre cada uno de los archivos 
+                test: /\.html$/,
+                loader: 'html-loader',
+                options:{
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use: [ MiniCssExtract.loader, 'css-loader' ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader',
+                options:{
+                    name: '[path][name].[ext]',
+                }
+            }
+
+        ]
+    },
+
+    optimization: {
+       
+    },
+
+    plugins: [
+        new HtmlWebpack({
+            title: 'Webpack App',
+            // filename: 's.html'
+            template: './src/index.html'
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "src/assets", to: "assets/" },
+            ]
+        })
+    ]
+}
